39ce293043afc4c7f767e03e6ee1739b2a5fd066
vishweshkadam <75071313+vishweshkadam@users.noreply.github.com>
2022-01-26 21:40:05 +0530

    Add files via upload

---
+++ bedroom.js
@@ -0,0 +1,35 @@
+img=""
+staus=""
+function preload(){
+    img=loadImage("BEDROOM.jpg")
+}
+function setup(){
+Canvas=createCanvas(500,400)
+Canvas.center()
+
+Object_Detect=ml5.objectDetector("cocossd",modelloaded);
+document.getElementById("satus").innerHTML="Status detecting object"
+
+}
+function modelloaded(){
+    console.log("model loaded")
+    staus=true;
+    Object_Detect.detect(img,gotresult)
+}
+function gotresult (error,result){
+    if(error){
+        console.log(error)
+    }
+
+object=result
+console.log(object)
+}
+
+function back(){
+    window.location="index.html"
+}
+
+
+function draw(){
+    image(img,0,0,500,400)
+}
